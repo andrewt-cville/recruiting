eb46045ce5af4d63eb1c90236bb521d9c3bde49c
CvilleTerp <andrewfthornton@gmail.com>
2021-04-07 18:30:13 -0400

    think i find the error
    need to set #default_exp at the top along with importing show_doc

--- recruiting/_nbdev.py
+++ recruiting/_nbdev.py
@@ -4,7 +4,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {}
 
-modules = ["None.py"]
+modules = ["AllAmerican.py"]
 
 doc_url = "https://andrewt-cville.github.io/recruiting/"
 
