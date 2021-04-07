b1c9ea4c2b5e5008ddbeecd6366562faae33ba55
CvilleTerp <andrewfthornton@gmail.com>
2021-04-07 18:20:21 -0400

    getting closer - processing another test for docs

---
+++ recruiting/None.py
@@ -0,0 +1,15 @@
+
+
+# Cell
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import time
+import json
+
+# Cell
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import time
+import json
